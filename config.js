c5f9986042421ffff0eb52fc693833af5a1e008f
Nasir <nasiruddinkhan07@gmail.com>
2018-10-28 11:50:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var configs = {};
 configs.applicationPort = 3001;
-configs.dbName = 'CrossoverVideosAssignment';
+configs.dbName = 'VideoPortal';
 configs.dbHost = 'localhost:27017';
 
 module.exports = configs;
